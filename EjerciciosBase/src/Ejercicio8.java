e42c477564f298f7747380c7d9ffa767a2ed1c37
carlosninacio <cigramirez@poligran.edu.co>
2026-02-18 15:11:30 -0500

    Definición matriz

--- EjerciciosBase/src/Ejercicio8.java
+++ EjerciciosBase/src/Ejercicio8.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Ejercicio8 {
+    public static void main(String[] args) {
 
-    // Arreglos 2D: suma de matrices n×m y verificación de matriz identidad (si es cuadrada).
-
-    
+        // Arreglos 2D: suma de matrices n×m y verificación de matriz identidad (si es cuadrada).
+        Scanner consola = new Scanner(System.in);
+        System.out.print("Introduzca el número de filas y columnas que desea para su matriz cuadrada: ");
+        int longitud = Integer.parseInt(consola.nextLine());
+        int[][] matriz = new int[longitud][longitud];
+    }
 }
