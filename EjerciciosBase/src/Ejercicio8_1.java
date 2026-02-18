b0d4f39b002f3f5bb9abe726db9d852d98a8bd11
carlosninacio <cigramirez@poligran.edu.co>
2026-02-18 15:55:07 -0500

    Versión 2 del Ejercicio 8. Crear y rellenar dos matrices y sus tamaños

---
+++ EjerciciosBase/src/Ejercicio8_1.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class Ejercicio8_1 {
+    public static void main(String[] args) {
+
+        // Matrices: sumar matrices n x m y verificar si son identidad (solo si es cuadrada)
+
+        Scanner consola = new Scanner(System.in);
+        System.out.print("Introduzca el número de filas de la primera matriz: ");
+        int filas = Integer.parseInt(consola.nextLine());
+        System.out.print("Introduzca el número de columnas de la primera matriz: ");
+        int columnas = Integer.parseInt(consola.nextLine());
+        int[][] matriz = new int[filas][columnas];
+
+        // Rellenar matriz
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j ++) {
+                System.out.print("Introduce un número para la posición [" + i + "] [" + j + "] -> ");
+                matriz[i][j] = Integer.parseInt(consola.nextLine());
+            }
+        }
+
+        // Recorrer matriz
+        for (int i = 0; i < filas; i++) {
+            System.out.println();
+            for (int j = 0; j < columnas; j ++) {
+                System.out.print(" [" + matriz[i][j] + "]");
+            }
+        }
+
+        System.out.print("Introduzca el número de filas de la segunda matriz: ");
+        filas = Integer.parseInt(consola.nextLine());
+        System.out.print("Introduzca el número de columnas de la segunda matriz: ");
+        columnas = Integer.parseInt(consola.nextLine());
+        int[][] matriz2 = new int[filas][columnas];
+
+        // Rellenar matriz
+        for (int i = 0; i < filas; i++) {
+            for (int j = 0; j < columnas; j ++) {
+                System.out.print("Introduce un número para la posición [" + i + "] [" + j + "] -> ");
+                matriz2[i][j] = Integer.parseInt(consola.nextLine());
+            }
+        }
+
+        // Recorrer matriz
+        for (int i = 0; i < filas; i++) {
+            System.out.println();
+            for (int j = 0; j < columnas; j ++) {
+                System.out.print(" [" + matriz2[i][j] + "]");
+            }
+        }
+
+
+        System.out.println(matriz.length);
+        System.out.println(matriz[0].length);
+
+
+    }
+}
