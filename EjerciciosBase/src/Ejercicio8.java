ff756e990a7594e4644a6488b4eaaaa166304f76
carlosninacio <cigramirez@poligran.edu.co>
2026-02-18 15:46:16 -0500

    Ejercicio 8 terminado

--- EjerciciosBase/src/Ejercicio8.java
+++ EjerciciosBase/src/Ejercicio8.java
@@ -3,7 +3,8 @@ import java.util.Scanner;
 public class Ejercicio8 {
     public static void main(String[] args) {
 
-        // Arreglos 2D: suma de matrices n×m y verificación de matriz identidad (si es cuadrada).
+        // Matrices, verificar si matriz es identidad
+
         Scanner consola = new Scanner(System.in);
         System.out.print("Introduzca el número de filas y columnas que desea para su matriz cuadrada: ");
         int longitud = Integer.parseInt(consola.nextLine());
@@ -19,11 +20,36 @@ public class Ejercicio8 {
 
         // Prueba: Recorrer matriz
         for (int i = 0; i < longitud; i++) {
-            System.out.println("");
+            System.out.println();
             for (int j = 0; j < longitud; j ++) {
                 System.out.print(" [" + matriz[i][j] + "]");
             }
         }
+
+        // Comprobar si es identidad
+        boolean esIdentidad = true;
+        for (int i = 0; i < longitud; i++) {
+            for (int j = 0; j < longitud; j ++) {
+                if (i == j) {
+                    if (matriz[i][j] != 1) {
+                        esIdentidad = false;
+                        break;
+                    }
+                } else {
+                    if (matriz[i][j] != 0) {
+                        esIdentidad = false;
+                        break;
+                    }
+                }
+            }
+            if (!esIdentidad) {
+                break;
+            }
+        }
+
+        System.out.println("");
+        String mensaje = (esIdentidad) ? "\nLa matriz es identidad" : "\nLa matriz NO es identidad";
+        System.out.println(mensaje);
 
     }
 }
