321ea827fd9af1a19b55ae51ce00844d6f9c35dd
carlosninacio <cigramirez@poligran.edu.co>
2026-02-21 19:33:41 -0500

    Ejercicio 11 terminado

--- EjerciciosBase/src/Ejercicio11/Main.java
+++ EjerciciosBase/src/Ejercicio11/Main.java
@@ -2,6 +2,10 @@ package Ejercicio11;
 
 public class Main {
     public static void main(String[] args) {
-        
+
+        Person persona1 = new Person("Carlos", 19);
+        System.out.println(persona1.toString());
+        persona1.birthday();
+        System.out.println(persona1.toString());
     }
 }
--- EjerciciosBase/src/Ejercicio11/Person.java
+++ EjerciciosBase/src/Ejercicio11/Person.java
@@ -1,4 +1,41 @@
 package Ejercicio11;
 
 public class Person {
+
+    private String name;
+    private int edad;
+
+    public Person (String name, int edad) {
+        this.name = name;
+        this.edad = edad;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", edad=" + edad +
+                '}';
+    }
+
+    public void birthday() {
+        System.out.println("Cumpliste 1 año, felicidades!");
+        this.edad = ++this.edad;
+    }
 }
