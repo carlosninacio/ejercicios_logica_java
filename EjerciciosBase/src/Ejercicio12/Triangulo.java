a5a29384f46d61da541bbaedb074357bcbf79264
carlosninacio <cigramirez@poligran.edu.co>
2026-03-03 19:28:31 -0500

    Clase triangulo

--- EjerciciosBase/src/Ejercicio12/Triangulo.java
+++ EjerciciosBase/src/Ejercicio12/Triangulo.java
@@ -1,4 +1,17 @@
 package Ejercicio12;
 
-public class Triangulo {
+public class Triangulo extends Forma {
+
+    private final double altura;
+    private final double base;
+
+    public Triangulo (double altura, double base) {
+        this.altura = altura;
+        this.base = base;
+    }
+
+    @Override
+    public double calcularArea() {
+        return (altura * base) / 2;
+    }
 }
