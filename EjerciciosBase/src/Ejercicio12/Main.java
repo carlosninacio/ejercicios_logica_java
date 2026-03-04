932df095160df770d53c239e22349f7650fde067
carlosninacio <cigramirez@poligran.edu.co>
2026-03-03 19:33:22 -0500

    Ejercicio 12 terminado
    Herencia, polimorfismo y lista para evaluar

--- EjerciciosBase/src/Ejercicio12/Main.java
+++ EjerciciosBase/src/Ejercicio12/Main.java
@@ -1,4 +1,25 @@
 package Ejercicio12;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
+    public static void main(String[] args) {
+        List<Forma> formas = new ArrayList<>();
+        Triangulo triangulo = new Triangulo(12.5,38);
+        Circulo circulo = new Circulo(2);
+        Circulo circulo2 = new Circulo(1.5);
+        formas.add(triangulo);
+        formas.add(circulo);
+        formas.add(circulo2);
+
+        double total = 0;
+        for (Forma f: formas) {
+            total += f.calcularArea();
+        }
+
+        System.out.println("Total: " + total);
+    }
+
 }
