068789328cb24dcc8b1d6235b847513cafa5bf0c
carlosninacio <cigramirez@poligran.edu.co>
2026-03-03 19:26:26 -0500

    Clase circulo

--- EjerciciosBase/src/Ejercicio12/Circulo.java
+++ EjerciciosBase/src/Ejercicio12/Circulo.java
@@ -1,4 +1,15 @@
 package Ejercicio12;
 
-public class Circulo {
+public class Circulo extends Forma {
+
+    private final double radio;
+
+    public Circulo(double radio) {
+        this.radio = radio;
+    }
+
+    @Override
+    public double calcularArea() {
+        return Math.PI * radio * radio;
+    }
 }
